c7fa313d3b0d77866568960db693959d45c0a0bb
Daniel Flynt <danielflynt1@gmail.com>
2018-03-13 09:42:30 -0400

    pylint compliant

--- game.py
+++ game.py
@@ -1,8 +1,9 @@
 import random
 import os
-words = ["phone", "cruise ship", "soccer", "movie", "vacation home", "road trip", "tortoise"]
+words = ["phone", "cruise ship", "soccer", "movie",
+         "vacation home", "road trip", "tortoise"]
 
-wordIndex = random.randint(0,6)
+wordIndex = random.randint(0, 6)
 word = words[wordIndex]
 wordSolved = False
 correctGuessedLetters = []
@@ -11,16 +12,23 @@ for i in range(0, len(word)):
         correctGuessedLetters.append(" ")
     else:
         correctGuessedLetters.append("_")
+
 print(" ".join(correctGuessedLetters))
+
 while not wordSolved:
     indexesContainingGuessedLetter = []
     choice = input("Enter a letter: ")
     if choice in word:
-        #add index of letter in word to the list if guessed letter is in the word
-        indexesContainingGuessedLetter = [i for i, x in enumerate(word) if x == choice]
+        # add index of letter in word to the list if
+        # guessed letter is in the word
+        indexesContainingGuessedLetter =
+        [i for i, x in enumerate(word) if x == choice]
+
         for x in range(0, len(indexesContainingGuessedLetter)):
-            #add correctly guessed letters to their corresponding index in the mystery word
+            # add correctly guessed letters to their
+            # corresponding index in the mystery word
             correctGuessedLetters[indexesContainingGuessedLetter[x]] = choice
+
     print(" ".join(correctGuessedLetters))
     if "_" not in correctGuessedLetters:
         print("Puzzle Solved!")
